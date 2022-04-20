c907ef6a711ec89a6b058745146d6b7158c1a2e1
Hashi Sen <matheuscostafer159@gmail.com>
2022-04-20 14:12:21 -0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,5 +1,5 @@
 
-var imagens = ['/images/image1.png','/images/image2.png'];
+var imagens = ['./images/image1.png','./images/image2.png'];
 var div_dotz = document.getElementById("div__dotz");
 
 var cont = 1;
@@ -39,22 +39,3 @@ function autoSlide() {
 
 addNovoDotz();
 window.onload = autoSlide;
-
-/*======================================================
-                    MENU MOBILE*/
-const btnMenu = document.getElementById('btn-menu');
-const btnInicio = document.getElementById('inicio');
-const btnProfi = document.getElementById('profissional');
-const btnInsti = document.getElementById('instituicao');
-const btnConsu = document.getElementById('consumidor')
-
-function viewMenu(){
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-btnMenu.addEventListener('click', viewMenu);
-btnInicio.addEventListener('click', viewMenu);
-btnProfi.addEventListener('click', viewMenu);
-btnInsti.addEventListener('click', viewMenu);
-btnConsu.addEventListener('click', viewMenu);
